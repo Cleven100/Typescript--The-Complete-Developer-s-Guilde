a78c04248abf81eba40a931c2540da2d91b8b8b4
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-08-01 13:23:07 -0300

    Catching Errors with Typescript

--- Secao1 - Getting Started with Typescript/index.ts
+++ Secao1 - Getting Started with Typescript/index.ts
@@ -2,7 +2,25 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+
+// Response.data has properties of:
 axios.get(url)
     .then(response => {
-      console.log(response.data);
+      const todo = response.data as Todo;
+
+      const ID = todo.id;
+      const title = todo.title;
+      const finished = todo.completed;
+
+      console.log(`
+        The Todo with ID: ${ID}
+        Has a title of: ${title}
+        Is it finished? ${finished}
+      `);
     })
